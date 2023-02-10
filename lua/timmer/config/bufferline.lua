fc48ffc518adc2f80ae1567d6020c17f47b85a8a
Timothy Kaufman <timkauf@gmail.com>
2023-02-10 14:58:33 -0800

    Create bufferline.lua

---
+++ lua/timmer/config/bufferline.lua
@@ -0,0 +1,32 @@
+require('bufferline').setup({
+    options = {
+        indicator = {
+            -- icon = '',
+            style = 'none',
+        },
+        color_icons = true,
+        show_close_icon = true,
+        show_buffer_close_icons = true,
+        show_buffer_icons = true,
+        show_buffer_default_icon = true,
+        tab_size = 0,
+        max_name_length = 25,
+        offsets = {
+            {
+                filetype = 'NvimTree',
+                text = '  Files',
+                highlight = 'StatusLine',
+                text_align = 'left',
+            },
+        },
+        separator_style = 'thin',
+        -- modified_icon = '*',
+        custom_areas = {
+            left = function()
+                return {
+                    { text = '     ', fg = '#8fff6d' },
+                }
+            end,
+        },
+    },
+})

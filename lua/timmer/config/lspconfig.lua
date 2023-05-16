ca50742bc9d1b8a8632d6672c23a010770b171b4
Tim Kaufman <tkaufman@box.com>
2023-05-16 15:50:03 -0700

    LSP mappings

--- lua/timmer/config/lspconfig.lua
+++ lua/timmer/config/lspconfig.lua
@@ -19,14 +19,14 @@ require('lspconfig').volar.setup({
 require('lspconfig').tailwindcss.setup({})
 
 -- Key mappings
-vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>)')
+vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>)')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>)')
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>)')
-vim.keymap.set('n', '<Leader>gd', '<cmd>lua vim.lsp_buf.definition()<CR>)')
-vim.keymap.set('n', '<Leader>gi', ':Telescope lsp_implementations<CR>)')
-vim.keymap.set('n', '<Leader>gr', ':Telescope lsp_references<CR>)')
-vim.keymap.set('n', '<Leader>K', '<cmd>lua vim.lsp.buf.hover()<CR>)')
-vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>)')
+vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp_buf.definition()<CR>)')
+vim.keymap.set('n', '<leader>gi', ':Telescope lsp_implementations<CR>)')
+vim.keymap.set('n', '<leader>gr', ':Telescope lsp_references<CR>)')
+vim.keymap.set('n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>)')
+vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>)')
 
 -- Sign config
 vim.fn.sign_define('DiagnosticSignError', {text = '', texthl = 'DiagnosticSignError'})
@@ -34,3 +34,9 @@ vim.fn.sign_define('DiagnosticSignWarn', {text = '', texthl = 'DiagnosticSign
 vim.fn.sign_define('DiagnosticSignInfo', {text = '', texthl = 'DiagnosticSignInfo'})
 vim.fn.sign_define('DiagnosticSignHint', {text = '', texthl = 'DiagnosticSignHint'})
 
+vim.diagnostic.config({
+    virtual_text =  true,
+    float = {
+        source = true,
+    }
+})

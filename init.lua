bf83fffa3e4f950d5e4e4a30d53e8fb464a371d5
Tim Kaufman <tkaufman@box.com>
2023-09-03 10:21:56 -0700

    Move globals stuff into init

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
+local g = vim.g
+
+-- Disable netrw in favor of nvim.tree
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+-- Leader
+g.mapleader = " "
+-- g.maplocalleader = " "
+-- g.localleader = "\\"
+
 -- Imports
-require('timmer/globals')
 require('timmer/options')
 require('timmer/keymaps')
 require('timmer/plugins')

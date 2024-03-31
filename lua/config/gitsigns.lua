2f929b0038fc422090b2326adacada777622dabe
Tim Kaufman <tkaufman@box.com>
2024-03-30 22:36:54 -0700

    Keymap conflict

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -14,6 +14,6 @@ vim.keymap.set('n', '<leader>gh', ':Gitsigns next_hunk<CR>')
 vim.keymap.set('n', '<leader>gH', ':Gitsigns prev_hunk<CR>')
 vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk<CR>')
 vim.keymap.set('n', '<leader>gS', ':Gitsigns undo_stage_hunk<CR>')
-vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>')
+vim.keymap.set('n', '<leader>gv', ':Gitsigns preview_hunk<CR>')
 vim.keymap.set('n', '<leader>gb', ':Gitsigns blame_line<CR>')
 
